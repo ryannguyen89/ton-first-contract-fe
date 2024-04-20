99f1c69bf489fbd0822721cab3c936f7bae55988
Ryan Nguyen <hoangnn89@outlook.com>
2024-04-21 06:37:30 +0700

    Workflow

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ function App() {
   const {
     contract_address,
     counter_value,
-    recent_sender,
-    owner_address,
     contract_balance,
     sendIncrement,
     sendDeposit,
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://ryannguyen89.github.io/ton-first-contract-fe/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
